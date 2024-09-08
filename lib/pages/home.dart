d26ff0fe68eac16f273d6b07720ca1faca317d44
TranHuuThais <huuthai10052004@gmail.com>
2024-09-08 16:52:35 +0700

    Initial commit

---
+++ lib/pages/home.dart
@@ -0,0 +1,127 @@
+import 'package:flutter/material.dart';
+import 'package:shoppingapp/widget/support_widget.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  List categories = [
+    "images/headphone_icon.png",
+    "images/laptop.png",
+    "images/watch.png",
+    "images/TV.png",
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xfff2f2f2),
+      body: Container(
+        margin: EdgeInsets.only(top: 45.0, right: 20.0, left: 20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Hey, Huuthai",
+                      style: AppWidget.boldTextFeildStyle(),
+                    ),
+                    Text(
+                      "Good Moring",
+                      style: AppWidget.LightTextFeildStyle(),
+                    ),
+                  ],
+                ),
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                  child: Image.asset(
+                    "images/boy.jpg",
+                    height: 80,
+                    width: 80,
+                    fit: BoxFit.cover,
+                  ),
+                )
+              ],
+            ),
+            SizedBox(
+              height: 30.0,
+            ),
+            Container(
+              decoration: BoxDecoration(
+                  color: Colors.white, borderRadius: BorderRadius.circular(10)),
+              width: MediaQuery.of(context).size.width,
+              child: TextField(
+                decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: "Search Products",
+                    hintStyle: AppWidget.LightTextFeildStyle(),
+                    prefixIcon: Icon(
+                      Icons.search,
+                      color: Colors.black,
+                    )),
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "Categories",
+                  style: AppWidget.semiBoldTextFeildStyle(),
+                ),
+                Text(
+                  "See all",
+                  style: TextStyle(
+                      color: Color(0xFFfd6f3e),
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.bold),
+                )
+              ],
+            ),
+            Container(
+              margin: EdgeInsets.only(left: 20.0),
+              height: 70,
+              child: ListView.builder(itemCount: categories.length,
+              shrinkWrap: true,
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (context, index)  {
+                return  CategoryTile(image: categories[index]);
+              }),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+  class CategoryTile extends StatelessWidget{
+    String image;
+    
+    CategoryTile ({required this.image});
+    @override
+    Widget build(BuildContext context){
+      return Container(
+        decoration: BoxDecoration(
+          color: Colors.white
+        ),
+        height: 90,
+        width: 90,
+        child: Column(children: [
+          Image.asset(image),
+        ],),
+      );
+    }
+  }
+
+
---
+++ lib/widget/support_widget.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class AppWidget {
+  static TextStyle boldTextFeildStyle() {
+    return TextStyle(
+        color: Colors.black, fontSize: 30.0, fontWeight: FontWeight.bold);
+  }
+
+  static TextStyle LightTextFeildStyle() {
+    return TextStyle(
+        color: Colors.black54, fontSize: 20.0, fontWeight: FontWeight.bold);
+  }
+
+  static TextStyle semiBoldTextFeildStyle() {
+    return TextStyle(
+        color: Colors.black, fontSize: 20.0, fontWeight: FontWeight.bold);
+  }
+}
